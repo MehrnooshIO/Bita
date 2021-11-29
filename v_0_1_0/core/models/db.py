df147f8f5f1eb3b14b03c181d8519683f77d6df7
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-11-29 12:47:30 +0330

    Change database address

--- v_0_1_0/core/models/db.py
+++ v_0_1_0/core/models/db.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine, engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
-engine = create_engine('sqlite:///blog.sqlite3')
+_DB_URI = 'mysql://root:fiFm3ZAzJddOihQN@services.gen1.chabokan.net:52782/rick'
+engine = create_engine(_DB_URI)
 sessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
