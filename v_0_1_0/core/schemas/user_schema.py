a2c45b5a327e973824a75bc13533c5011f58d02c
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-11-29 13:33:17 +0330

    Modify user_schema

--- v_0_1_0/core/schemas/user_schema.py
+++ v_0_1_0/core/schemas/user_schema.py
@@ -2,11 +2,8 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 
-class PostSchema(BaseModel):
-    title: str
-    author: str
-    content: str
-    preview: str
-    tags: Optional[List[str]] = None
-    created_at: str
-    last_updated_at: str
+class UserCreationSchema(BaseModel):
+    name: str
+    username: str
+    email: str
+    password: str
