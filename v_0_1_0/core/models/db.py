4a70128bc6e4d609543c5c6365762b053dc2b618
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-12-02 05:03:58 -0500

    Change _DB_URI to PostgrSQL

--- v_0_1_0/core/models/db.py
+++ v_0_1_0/core/models/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-_DB_URI = 'mysql://root:fiFm3ZAzJddOihQN@services.gen1.chabokan.net:52782/rick'
+_DB_URI = 'postgresql://root:JhE7DtbmnTdKQ5Sz@services.gen1.chabokan.net:15947/anna'
 engine = create_engine(_DB_URI)
 sessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
