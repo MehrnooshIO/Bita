82f270d6a3a97dbc52528098055e05d849548cd3
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-12-02 10:32:41 -0500

    Implement get users list from db

--- v_0_1_0/core/models/crud.py
+++ v_0_1_0/core/models/crud.py
@@ -11,8 +11,18 @@ from v_0_1_0.core.models.user_model import User
 
 # Create new user
 def create_user_db(db: Session, user: UserCreationSchema):
+    """
+    Create new user in database and return the id of that new record
+    """
     new_user = User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user.id
+
+
+def get_users_db(db:Session):
+    """
+    Retrive list of registered useres from database
+    """
+    return db.query(User).all()
