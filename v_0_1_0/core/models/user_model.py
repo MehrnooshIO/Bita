9a1a4644ba95f7151c710b83a98b3286c199d3a2
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-12-02 05:12:56 -0500

    Modify to remove relation with posts table temporary

--- v_0_1_0/core/models/user_model.py
+++ v_0_1_0/core/models/user_model.py
@@ -4,8 +4,7 @@ import sys
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(PROJECT_ROOT, '../../'))
 
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, DateTime
 from v_0_1_0.core.models.db import Base
 from datetime import datetime
 
@@ -18,4 +17,4 @@ class User(Base):
     email = Column(String(50), nullable=False)
     password = Column(String(50), nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
-    posts = relationship('Post', backref='user', default= None)
+    # posts = relationship('Post', backref='user', lazy=True)
