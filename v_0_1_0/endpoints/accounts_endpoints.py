a6e1300649f648fbae14f31d524a9d6c8f161122
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-12-02 05:07:04 -0500

    Modify register end point to acctually save data to db

--- v_0_1_0/endpoints/accounts_endpoints.py
+++ v_0_1_0/endpoints/accounts_endpoints.py
@@ -8,12 +8,15 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from v_0_1_0.core.schemas.user_schema import UserCreationSchema
 from v_0_1_0.core.models import crud
-from v_0_1_0.core.models.db import SessionLocal
+from v_0_1_0.core.models.db import Base, sessionLocal, engine
+
+
+Base.metadata.create_all(bind=engine)
 
 
 # Dependency
 def get_db():
-    db = SessionLocal()
+    db = sessionLocal()
     try:
         yield db
     finally:
@@ -30,10 +33,12 @@ def create_user_form():
     pass
 
 
+# TODO: #3 Change try - except block to cover more db failiour cases
 @accounts.post("/register", status_code=201)
 def create_account(user: UserCreationSchema, db: Session=Depends(get_db)):
     try:
-        return crud.create_user_db(db, user)
+        id = crud.create_user_db(db, user)
+        return {"id": id}
     except Exception as e:
         raise HTTPException(
             status_code=424,
