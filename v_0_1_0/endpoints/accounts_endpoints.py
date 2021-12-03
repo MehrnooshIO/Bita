933e7ca6c2f9ca0da32c7a8874a859f426488972
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-12-03 13:37:43 -0500

    Change import method to relative import

--- v_0_1_0/endpoints/accounts_endpoints.py
+++ v_0_1_0/endpoints/accounts_endpoints.py
@@ -1,17 +1,8 @@
-import os
-import sys
-
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, os.path.join(PROJECT_ROOT, '../../'))
-
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from v_0_1_0.core.schemas.user_schema import UserCreationSchema
 from v_0_1_0.core.models import crud
-from v_0_1_0.core.models.db import Base, sessionLocal, engine
-
-
-Base.metadata.create_all(bind=engine)
+from v_0_1_0.core.models.db import sessionLocal
 
 
 # Dependency
