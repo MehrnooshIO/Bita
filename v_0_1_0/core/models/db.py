f629ccd8a24975d1911429081767a5f744f0bfed
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-12-03 13:38:57 -0500

    Add reading config file

--- v_0_1_0/core/models/db.py
+++ v_0_1_0/core/models/db.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine, engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from configparser import ConfigParser
 
-_DB_URI = 'postgresql://root:JhE7DtbmnTdKQ5Sz@services.gen1.chabokan.net:15947/anna'
+
+config = ConfigParser()
+config.read(".env")
+_DB_URI = f"postgresql://{config['DATABASE']['USER']}:{config['DATABASE']['PASSWORD']}@{config['DATABASE']['HOST']}:{config['DATABASE']['PORT']}/{config['DATABASE']['NAME']}"
+
 engine = create_engine(_DB_URI)
 sessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
-
 Base.metadata.create_all(engine)
