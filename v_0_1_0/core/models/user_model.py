3bb5e51cca9637dc64c3076f96a5bb5dd932b9ae
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-12-03 16:19:58 -0500

    Add post model

--- v_0_1_0/core/models/user_model.py
+++ v_0_1_0/core/models/user_model.py
@@ -1,20 +1,27 @@
-import os
-import sys
-
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, os.path.join(PROJECT_ROOT, '../../'))
-
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, ARRAY
+from sqlalchemy.orm import relationship
 from v_0_1_0.core.models.db import Base
 from datetime import datetime
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
     password = Column(String(50), nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
-    # posts = relationship('Post', backref='user', lazy=True)
+    posts = relationship("Post", back_populates="user")
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    content = Column(String(1000), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    last_updated_at = Column(DateTime, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="posts")
+    tags = ARRAY(String(100))
