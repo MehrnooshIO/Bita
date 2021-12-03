88d9271c26d112c6a9cd5b69131f27be0fa63cd0
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-12-03 13:38:23 -0500

    Change import method and add test for get users

--- tests/v_0_1_0/test_user.py
+++ tests/v_0_1_0/test_user.py
@@ -1,12 +1,7 @@
-import os
-import sys
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(PROJECT_ROOT, '../../'))
-
 from fastapi.testclient import TestClient
 from main import app
-
+from typing import List
+from v_0_1_0.core.models.user_model import User
 
 client = TestClient(app)
 
@@ -18,3 +13,9 @@ def test_create_user():
         'password': 'test_password'
     })
     assert response.status_code == 201
+    assert response.json().get('id') is not None
+
+
+def test_get_users():
+    response = client.get('/accounts/users')
+    assert isinstance(response.json(), List)
